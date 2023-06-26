3da715ef6ad233919b87ddf95132e89d3bf0f8b5
Baltazar Ramirez <BaltaRT25@gmail.com>
2023-06-26 09:49:15 -0600

    Agregar proyecto inicial

---
+++ PokemonAPI/Controllers/PokemonController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PokemonShared.DTO;
+using System.Text.Json;
+
+namespace PokemonAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokemonController : ControllerBase
+    {
+        [HttpGet("ObtenerPorNombre/{nombre}")]
+        public async Task<PokemonDTO> ObtenerPorNombre(string nombre)
+        {
+            var pokemon = new PokemonDTO();
+
+            var peticion = new HttpClient();
+            var respuesta = await peticion.GetAsync($"https://pokeapi.co/api/v2/pokemon/{nombre}");
+
+            if(respuesta.IsSuccessStatusCode)
+            {
+                var contenido = await respuesta.Content.ReadAsStringAsync();
+                pokemon = JsonSerializer.Deserialize<PokemonDTO>(contenido);
+
+                if (pokemon != null)
+                {
+                    pokemon.image = $"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{pokemon.id}.png";
+                }
+
+            }
+            else if ((int)respuesta.StatusCode == 404)
+            {
+                pokemon.name = "Not found.";
+               
+            }
+
+            return pokemon;
+        }
+    }
+}
---
+++ PokemonShared/DTO/PokemonDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonShared.DTO
+{
+    public class PokemonDTO
+    {
+        public int id { get; set; } = 0;
+        public string name { get; set; } = string.Empty;
+        public int weight { get; set; } = 0;
+        public int height { get; set; } = 0;
+        public string image { get; set; } = string.Empty;
+    }
+}
